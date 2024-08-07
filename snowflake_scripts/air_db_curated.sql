4faa926de164c661c58e04fd0bfb77c8b1ca2a46
halikowski <158446642+halikowski@users.noreply.github.com>
2024-08-07 19:28:47 +0200

    Update air_db_curated.sql
    Added stored procedure load_cities_data and cities_pop table

--- snowflake_scripts/air_db_curated.sql
+++ snowflake_scripts/air_db_curated.sql
@@ -3,6 +3,9 @@ use warehouse transform_wh;
 use role sysadmin;
 use schema curated;
 
+
+ALTER SESSION SET TIMEZONE = 'Europe/Warsaw';
+
 -- dynamic table for pollution data
 CREATE OR REPLACE DYNAMIC TABLE curated_pollution
 TARGET_LAG = 'DOWNSTREAM'
@@ -91,3 +94,73 @@ AS
 
 
 SELECT * FROM curated_weather
+
+-- table for population data in polish cities over 100k population
+CREATE OR REPLACE TABLE cities_pop (
+    city STRING,
+    city_ascii STRING,
+    lat FLOAT,
+    lon FLOAT,
+    country_code STRING,
+    population INT,
+    timezone STRING,
+    country STRING
+)
+
+-- copy file from github repo to plain internal stage in order for the load_cities_data procedure to work
+-- (this sadly does not work inside stored procedure)
+COPY FILES
+        INTO @air_db.raw.internal_stage/population/
+        FROM @air_db.raw.polish_pollution_repo/branches/main/PLcities_over_100k.csv
+
+        
+-- Procedure for copying the population data from internal stage file to cities_pop table
+CREATE OR REPLACE PROCEDURE load_cities_data()
+RETURNS STRING
+LANGUAGE SQL
+EXECUTE AS CALLER
+AS
+$$
+BEGIN
+    -- creating a temporary staging table for upsert operation on cities_pop table
+    CREATE OR REPLACE TEMP TABLE staging_cities (
+        city STRING,
+        city_ascii STRING,
+        lat FLOAT,
+        lon FLOAT,
+        country_code STRING,
+        population INT,
+        timezone STRING,
+        country STRING
+    );
+    
+    -- loading data to the staging table
+    COPY INTO staging_cities
+    FROM @air_db.raw.internal_stage/population/PLcities_over_100k.csv
+    FILE_FORMAT = raw.my_csv_format
+    ON_ERROR = 'CONTINUE';
+
+    -- upsert operation on the target table (cities_pop)
+    MERGE INTO cities_pop AS target
+    USING staging_cities AS source
+    ON target.city = source.city AND target.lat = source.lat AND target.lon = source.lon
+    WHEN MATCHED THEN
+        UPDATE SET 
+            city_ascii = source.city_ascii,
+            country_code = source.country_code,
+            population = source.population,
+            timezone = source.timezone,
+            country = source.country
+    WHEN NOT MATCHED THEN
+        INSERT (city, city_ascii, lat, lon, country_code, population, timezone, country)
+        VALUES (source.city, source.city_ascii, source.lat, source.lon, source.country_code, source.population, source.timezone, source.country);
+
+    RETURN 'Cities population data load completed successfully';
+END;
+$$;
+
+
+call load_cities_data();
+
+
+select * from cities_pop;
