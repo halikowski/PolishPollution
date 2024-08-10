509ecb6fb1a53c32ce81abc8d6e5bf1bace5c48e
Mateusz <mat.halikowski@gmail.com>
2024-08-10 18:57:13 +0200

    Added logging

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import boto3
 import os
 import datetime
 import pytz
+import logging
+
+logging.basicConfig(level=logging.DEBUG, encoding='utf-8')
 
 # Get AWS credentials
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
@@ -24,13 +27,10 @@ cities_data = json.loads(cities)
 # Extract coordinates - as latitudes and longitudes separately, for API calling on each pair
 lats = [city['Latitude'] for city in cities_data]
 lons = [city['Longitude'] for city in cities_data]
-print(f'LATS: {lats}')
-print(f'LONS: {lons}')
 
 # Empty arrays to gather all API outputs
 aq_data = []
 weather_data = []
-
 
 def call_openweather_api(endpoint,res_array,coords):
     """
@@ -51,14 +51,16 @@ def call_openweather_api(endpoint,res_array,coords):
             url = f'http://api.openweathermap.org/data/2.5/{endpoint}?units=metric&lat={lat}&lon={lon}&appid={OPENWEATHER_API_KEY}'
         else:
             url = f'http://api.openweathermap.org/data/2.5/{endpoint}?lat={lat}&lon={lon}&appid={OPENWEATHER_API_KEY}'
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
+        try:
+            response = requests.get(url, headers=headers)
             data = response.json()
             # swap response's coords with the original input coords from .csv file
             data['coord']['lat'] = lat
             data['coord']['lon'] = lon
             res_array.append(data)
-            print('Successfully fetched api data')
+            logging.info('Successfully fetched api data')
+        except Exception as e:
+            logging.info(f'An error occured while calling the API: {e}')
     wrapped_array = {"results": res_array}
     json_file = json.dumps(wrapped_array, ensure_ascii=False, indent=4).encode('utf-8')
 
@@ -67,7 +69,6 @@ def call_openweather_api(endpoint,res_array,coords):
 
 aq_file = call_openweather_api('air_pollution',aq_data,zip(lats,lons))
 weather_file = call_openweather_api('weather',weather_data,zip(lats,lons))
-#
 
 def send_to_s3(file_name,file_body, directory):
     """
@@ -83,10 +84,10 @@ def send_to_s3(file_name,file_body, directory):
     s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
     bucket_name = 'mateuszairqualitydata'
     try:
-         s3.put_object(Bucket=bucket_name, Key=f'raw/{directory}/{CURRENT_TIMESTAMP}_{file_name}', Body=file_body)
-         print(f"Data successfully uploaded to s3://{bucket_name}/raw/{directory}/{CURRENT_TIMESTAMP}_{file_name}")
-    except:
-        print(f"Failed to upload data")
+        s3.put_object(Bucket=bucket_name, Key=f'raw/{directory}/{CURRENT_TIMESTAMP}_{file_name}', Body=file_body)
+        logging.info(f"Data successfully uploaded to s3://{bucket_name}/raw/{directory}/{CURRENT_TIMESTAMP}_{file_name}")
+    except Exception as e:
+        logging.info(f"Failed to upload data: {e}")
 
 
 send_to_s3('aq_data.json', aq_file,'pollution')
